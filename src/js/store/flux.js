910c7eda151943d43d43ff8505c107b7dea521c9
TulioPortela <156851372+Tulio-Portela@users.noreply.github.com>
2024-06-27 16:10:50 +0000

    finalizado

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -18,41 +18,50 @@ const getState = ({ getStore, getActions, setStore }) => {
 		contact: [],
 	  },
 	  actions: {
-		// Use getActions to call a function within a fuction
 		exampleFunction: () => {
 		  getActions().changeColor(0, "green");
 		},
-		loadSomeData: () => {
-		  /**
-					  fetch().then().then(data => setStore({ "foo": data.bar }))
-				  */
-		},
+		loadSomeData: () => {},
 		changeColor: (index, color) => {
-		  //get the store
 		  const store = getStore();
-  
-		  //we have to loop the entire demo array to look for the respective index
-		  //and change its color
 		  const demo = store.demo.map((elm, i) => {
 			if (i === index) elm.background = color;
 			return elm;
 		  });
-  
-		  //reset the global store
 		  setStore({ demo: demo });
 		},
 		getContacts: async function () {
 		  let store = getStore();
 		  try {
-			const response = await fetch(
-			  `${store.baseURL}/agenda/Jssolar`
-			);
-			// waits until the request completes...
-			console.log(response);
+			const response = await fetch(`${store.baseURL}/agenda/Jssolar`);
 			if (response.ok) {
 			  let data = await response.json();
 			  setStore({ contactList: data });
-			  console.log(data);
+			} else if (response.status === 404) {
+			  await getActions().createAgenda();
+			}
+		  } catch (error) {
+			console.log(error);
+		  }
+		},
+		createAgenda: async function () {
+		  let store = getStore();
+		  try {
+			const response = await fetch(`${store.baseURL}`, {
+			  method: "POST",
+			  headers: {
+				"Content-Type": "application/json",
+			  },
+			  body: JSON.stringify({
+				"agenda_slug": "Jssolar",
+				"full_name": "First Contact",
+				"email": "first.contact@example.com",
+				"phone": "123-456-7890",
+				"address": "123 Main St"
+			  }),
+			});
+			if (response.ok) {
+			  getActions().getContacts();
 			}
 		  } catch (error) {
 			console.log(error);
@@ -62,12 +71,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 		  let store = getStore();
 		  try {
 			const response = await fetch(`${store.baseURL}/${id}`);
-			// waits until the request completes...
-			console.log(response);
 			if (response.ok) {
 			  let data = await response.json();
 			  setStore({ contact: data });
-			  console.log(data);
 			}
 		  } catch (error) {
 			console.log(error);
@@ -134,3 +140,4 @@ const getState = ({ getStore, getActions, setStore }) => {
   };
   
   export default getState;
+  
